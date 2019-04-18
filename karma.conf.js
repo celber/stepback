731bb12d4c4949a4013203bcd4393ac47d134a17
Dariusz Jakubowski <celber@me.com>
2019-04-18 13:04:11 +0200

    change karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
     config.set({
       browsers: ['PhantomJS'],
-      reporters: ['dots', 'coverage'],
+      reporters: ['progress', 'coverage'],
       preprocessors: {
         'src/**/*.js': ['coverage']
       },
