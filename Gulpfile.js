0f5f60d0db20fd0245c5f5b2bc06cc9cc104d433
Dariusz Jakubowski <celber@me.com>
2019-06-27 13:03:11 +0200

    Added homepage

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,11 +9,33 @@ var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var postcssCustomSelectors = require('postcss-custom-selectors');
-
+var browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
-gulp.task('serve-playground', serve({root: ['./playground', './dist']}));
+// Compile sass into CSS & auto-inject into browsers
+exports['serve:homepage:sass'] = function() {
+  return gulp.src("./homepage/scss/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("./homepage/css"))
+      .pipe(browserSync.stream());
+};
+
+
+// gulp.task('serve:playground', serve({root: ['./playground', './dist']}));
+// gulp.task('serve:docs', serve({root: ['./docs']}));
+exports['serve:homepage'] = function() {
+
+  exports['serve:homepage:sass']();
+
+  browserSync.init({
+      server: "./homepage"
+  });
+
+  gulp.watch("./homepage/scss/*.scss", exports['serve:homepage:sass']);
+  gulp.watch("./homepage/*.html").on('change', browserSync.reload);
+};
+
 
 var fileList = [
   './src/Core.js',
@@ -27,7 +49,7 @@ var fileList = [
 ];
 
 
-function _concatJS () {
+function concatJS () {
   return gulp
     .src(fileList)
     .pipe(sourcemaps.init())
@@ -39,10 +61,8 @@ function _concatJS () {
     .pipe(gulp.dest('./dist/'));
 }
 
-gulp.task('concatJS', _concatJS);
 
-
-function _buildSCSS () {
+function buildSCSS () {
   return gulp.src('./src/**/*.scss')
   .pipe(postcss({modules: true}))
   .pipe(
@@ -57,50 +77,51 @@ function _buildSCSS () {
   .pipe(sourcemaps.write());
 };
 
-gulp.task('buildSCSS', _buildSCSS);
 
-function _minifyJS() {
+function minifyJS() {
   return gulp.src(['./dist/sb.js'])
     .pipe(minify())
     .pipe(gulp.dest('dist'))
 }
 
-gulp.task('minifyJS', _minifyJS);
+function buildJS() {
+  return gulp.series(concatJS, minifyJS)
+}
 
-gulp.task('buildJS', gulp.series('concatJS', 'minifyJS'));
-
+exports.buildJS = buildJS;
 
 /**
  * Run test once and exit
  */
 
-function _test(done) {
+function test(done) {
   new KarmaServer({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
   }, done).start();
 }
-gulp.task('test', _test);
+
+exports.test = test;
 
 /**
  * Watch for file changes and re-run tests on each change
  */
-gulp.task('tdd', function _test(done) {
+function tdd(done) {
   new KarmaServer({
     configFile: __dirname + '/karma.conf.js',
     singleRun: false
   }, done).start();
-});
+};
+exports.tdd = tdd;
 
+exports.build = gulp.series(buildSCSS, buildJS);
 
-gulp.task('build', gulp.series('buildSCSS','buildJS'));
+exports.default = gulp.series(buildSCSS, buildJS);
 
-gulp.task('default', gulp.series('buildSCSS','buildJS'));
-
-gulp.task('watch', function (done) {
+exports.watch = function watch(done) {
   watch(fileList.concat(['./src/**/*.scss']), function(cb) {
     console.log("Changes detected!");
-    gulp.series('buildSCSS','buildJS')(cb);
+    gulp.series(buildSCSS, buildJS)(cb);
     //cb();
   });
-});
+};
