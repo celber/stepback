646913cdf63fdadf1224246d01a23e5c8e532f52
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-31 15:01:20 +0000

    Deploy celber/stepback to github.com/celber/stepback.git:gh-pages

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (ctx) {
-    return { 
-        parser: 'postcss-scss',
-        plugins: {
-            'postcss-import': { root: ctx.file.dirname }
-        }
-    }
-  }
--- src/Store.js
+++
@@ -1,3 +0,0 @@
-Sb.Store = function () {
-  
-}
